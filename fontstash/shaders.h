6df04deab44efeab0db2699be6ac9f392e2ad41c
Karim Naaji <karim.naaji@gmail.com>
2015-01-14 11:11:44 -0500

    fix shader issues

--- fontstash/shaders.h
+++ fontstash/shaders.h
@@ -18,7 +18,7 @@ precision mediump float;
 #endif
 
 attribute lowp float a_fsid;
-attribute vec4 a_position;
+attribute vec2 a_position;
 attribute vec2 a_texCoord;
 
 uniform sampler2D u_transforms;
@@ -26,8 +26,8 @@ uniform lowp vec2 u_tresolution;
 uniform lowp vec2 u_resolution;
 uniform mat4 u_proj;
 
-varying vec2 f_uv;
-varying float f_alpha;
+varying vec2 v_uv;
+varying float v_alpha;
 
 #define PI 3.14159
 
@@ -42,10 +42,10 @@ varying float f_alpha;
  * Converts (i, j) pixel coordinates to the corresponding (u, v) in
  * texture space. The (u,v) targets the center of pixel
  */
-vec2 ij2uv(float i, float j, float w, float h) {
+vec2 ij2uv(float _i, float _j, float _w, float _h) {
     return vec2(
-        (2.0*i+1.0) / (2.0*w),
-        (2.0*j+1.0) / (2.0*h)
+        (2.0*_i+1.0) / (2.0*_w),
+        (2.0*_j+1.0) / (2.0*_h)
     );
 }
 
@@ -53,9 +53,9 @@ vec2 ij2uv(float i, float j, float w, float h) {
  * Decodes the id and find its place for its transform inside the texture
  * Returns the (i,j) position inside texture
  */
-vec2 id2ij(int fsid, float w) {
-    float i = mod(float(fsid * 2), (w * 0.5));
-    float j = floor(float(fsid * 2) / (w * 0.5));
+vec2 id2ij(int _fsid, float _w) {
+    float i = mod(float(_fsid * 2), _w);
+    float j = floor(float(_fsid * 2) / _w);
     return vec2(i, j);
 }
 
@@ -69,11 +69,12 @@ void main() {
     vec4 tdata = texture2D(u_transforms, uv1);
     vec4 tdataPrecision = texture2D(u_transforms, uv2);
 
-    float precisionScale = 1.0/255.0;
+    float txe = u_resolution.x / 255.0; // max error on x
+    float txy = u_resolution.y / 255.0; // max error on y
 
     // transforms from [0..1] to [0..resolution] and add lost precision
-    tx = u_resolution.x * (tx + txp * precisionScale);
-    ty = u_resolution.y * (ty + typ * precisionScale);
+    tx = u_resolution.x * tx + (txp * txe);
+    ty = u_resolution.y * ty + (typ * txy);
 
     // scale from [0..1] to [0..2pi]
     theta = theta * 2.0 * PI;
@@ -85,14 +86,14 @@ void main() {
     vec4 p = vec4(
         a_position.x * ct - a_position.y * st + tx,
         a_position.x * st + a_position.y * ct + ty,
-        a_position.z,
-        a_position.w
+        0.0,
+        1.0
     );
-
+    
     gl_Position = u_proj * p;
-
-    f_uv = a_texCoord;
-    f_alpha = alpha;
+    
+    v_uv = a_texCoord;
+    v_alpha = alpha;
 }
 
 )END";
@@ -114,10 +115,10 @@ uniform lowp float u_mixFactor;
 #define minInsideD  u_sdfParams.z
 #define maxInsideD  u_sdfParams.w
 
-varying vec2 f_uv;
+varying vec2 v_uv;
 
 void main(void) {
-    float distance = texture2D(u_tex, f_uv).a;
+    float distance = texture2D(u_tex, v_uv).a;
     vec4 inside = smoothstep(minInsideD, maxInsideD, distance) * u_color;
     vec4 outline = smoothstep(minOutlineD, maxOutlineD, distance) * u_outlineColor;
 
@@ -135,12 +136,12 @@ precision mediump float;
 uniform lowp sampler2D u_tex;
 uniform lowp vec3 u_color;
 
-varying vec2 f_uv;
-varying float f_alpha;
+varying vec2 v_uv;
+varying float v_alpha;
 
 void main(void) {
-    vec4 texColor = texture2D(u_tex, f_uv);
-    gl_FragColor = vec4(u_color.rgb, texColor.a * f_alpha);
+    vec4 texColor = texture2D(u_tex, v_uv);
+    gl_FragColor = vec4(u_color.rgb, texColor.a * v_alpha);
 }
 
 )END";
