f831e9c6d846d8ec9ed03d085d25c481138f2c17
Karim Naaji <karim.naaji@gmail.com>
2015-06-04 17:03:20 -0400

    update ios

--- examples/ios/FontstashiOS/ViewController.mm
+++ examples/ios/FontstashiOS/ViewController.mm
@@ -102,7 +102,7 @@
     glfonsTransform(fs, textIds[0], width / 2.0, height / 2.0, cos(t) * 0.5, cos(t) * 0.5 + 0.5);
     glfonsTransform(fs, textIds[4], width / 2.0, height / 2.0 - 200.0 + cos(t) * 20.0, 0.0, 1.0);
     // push transforms to gpu
-    glfonsUpdateTransforms(fs);
+    glfonsUpdateBuffer(fs);
     glfonsBindBuffer(fs, textBuffer);
     
     glfonsSetColor(fs, glfonsRGBA(0.0, 0.0, 100.0 * (cos(t) * 0.5 + 0.5), 255.0));
@@ -169,10 +169,7 @@
     }
     
     // push transforms of currently bound buffer buffer to gpu
-    glfonsUpdateTransforms(fs);
-    
-    // upload rasterized data of currently bound buffer to gpu
-    glfonsUpload(fs);
+    glfonsUpdateBuffer(fs);
 }
 
 - (void)deleteFontContext
